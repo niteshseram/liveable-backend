80fd9f78e13a3f4ad3655b88f3712f154bff18af
S Nitesh Singh <niteshseram@gmail.com>
2021-05-20 13:19:31 +0530

    adding product model

---
+++ models/product.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+const { Schema, ObjectId } = mongoose;
+
+const productSchema = Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      maxlength: 32,
+    },
+    description: {
+      type: String,
+      trim: true,
+      required: true,
+      maxlength: 2000,
+    },
+    price: {
+      type: Number,
+      required: true,
+      maxlength: 32,
+      trim: true,
+    },
+    category: {
+      type: ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    stock: {
+      type: Number,
+    },
+    sold: {
+      type: Number,
+      default: 0,
+    },
+    photo: {
+      data: Buffer,
+      contentType: String,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Product", productSchema);
