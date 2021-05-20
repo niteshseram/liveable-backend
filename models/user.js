84e6eaf6199c5ba1593ec704288584204c1ea304
S Nitesh Singh <niteshseram@gmail.com>
2021-05-20 12:51:53 +0530

    adding user model

---
+++ models/user.js
@@ -0,0 +1,71 @@
+import mongoose from "mongoose";
+import { createHmac } from "crypto";
+import { v1 as uuidv1 } from "uuid";
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    maxlength: 32,
+    trim: true,
+  },
+  lastname: {
+    type: String,
+    maxlength: 32,
+    trim: true,
+  },
+  email: {
+    type: String,
+    trim: true,
+    required: true,
+    unique: true,
+  },
+  userInfo: {
+    type: String,
+    trim: true,
+  },
+  //TODO: come back here
+  encry_password: {
+    type: String,
+    trim: true,
+  },
+  salt: String,
+  role: {
+    type: Number,
+    default: 0,
+  },
+  purchases: {
+    type: Array,
+    default: [],
+  },
+});
+
+userSchema
+  .virtual("password")
+  .set(function (password) {
+    this._password = password;
+    this.salt = uuidv1();
+    this.encry_password = this.securePassword(password);
+  })
+  .get(function () {
+    return this._password;
+  });
+
+userSchema.method = {
+  authenticate: function (plainpassword) {
+    return this.securePassword(plainpassword) === this.encry_password;
+  },
+  securePassword: function (plainpassword) {
+    if (!plainpassword) return "";
+    try {
+      return createHmac("sha256", this.salt)
+        .update(plainpassword)
+        .digest("hex");
+    } catch (err) {
+      return "";
+    }
+  },
+};
+
+module.exports = mongoose.model("User", userSchema);
