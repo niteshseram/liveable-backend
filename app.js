3c8c2735b12c5a55f6dba9ac0484a9001a13a3ad
S Nitesh Singh <niteshseram@gmail.com>
2021-05-21 17:30:10 +0530

    adding category routes and controller

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const cors = require("cors");
 //My Routes
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
+const categoryRoutes = require("./routes/category");
 
 //DB Connection
 mongoose
@@ -31,6 +32,7 @@ app.use(cors());
 //Routes
 app.use("/api", authRoutes);
 app.use("/api", userRoutes);
+app.use("/api", categoryRoutes);
 
 //PORT
 const port = process.env.PORT || 8000;
