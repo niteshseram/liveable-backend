0bc8a44c4474bdfdf7f56cd6295257941ce875cf
S Nitesh Singh <niteshseram@gmail.com>
2021-05-21 09:32:38 +0530

    adding validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,21 @@
 const express = require("express");
+const { check } = require("express-validator");
 const router = express.Router();
 const { signout, signup } = require("../controllers/auth");
 
-router.post("/signup", signup);
+router.post(
+  "/signup",
+  [
+    check("name")
+      .isLength({ min: 3 })
+      .withMessage("Name must be atleast 3 chars long"),
+    check("email").isEmail().withMessage("Email is required"),
+    check("password")
+      .isLength({ min: 3 })
+      .withMessage("Password must be atleast 3 chars long"),
+  ],
+  signup
+);
 router.get("/signout", signout);
 
 module.exports = router;
