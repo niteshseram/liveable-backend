b38bdb9528317bbdb2d218429ea651eb8edd9b0b
S Nitesh Singh <niteshseram@gmail.com>
2021-05-22 09:31:29 +0530

    adding status property

--- models/order.js
+++ models/order.js
@@ -21,6 +21,11 @@ const orderSchema = Schema(
       type: Number,
     },
     address: String,
+    status: {
+      type: String,
+      default: "Received",
+      enum: ["Cancelled", "Delivered", "Shipped", "Processing", "Received"],
+    },
     updated: Date,
     user: {
       type: ObjectId,
