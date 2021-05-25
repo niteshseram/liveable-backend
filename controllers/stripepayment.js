a7f27e0e51ba87e785d98e6e4e866057ee715557
S Nitesh Singh <niteshseram@gmail.com>
2021-05-25 12:11:36 +0530

    adding strip payment api

---
+++ controllers/stripepayment.js
@@ -0,0 +1,43 @@
+const stripe = require("stripe")(process.env.STRIPE_KEY);
+const { v4: uuid } = require("uuid");
+
+exports.makepayment = (req, res) => {
+  const { products, token } = req.body;
+  console.log("PRODUCTS ", products);
+
+  let amount = products.reduce((total, product) => {
+    return total + product.price;
+  }, 0);
+
+  const idempotencyKey = uuid();
+
+  return stripe.customers
+    .create({
+      email: token.email,
+      source: token.id,
+    })
+    .then((customer) => {
+      stripe.charges
+        .create(
+          {
+            amount: amount * 100,
+            currency: "inr",
+            customer: customer.id,
+            receipt_email: token.email,
+            description: "a test account",
+            shipping: {
+              name: token.card.name,
+              address: {
+                line1: token.card.address_line1,
+                line2: token.card.address_line2,
+                city: token.card.address_city,
+                country: token.card.address_country,
+              },
+            },
+          },
+          { idempotencyKey }
+        )
+        .then((result) => res.status(200).json(result))
+        .catch((err) => console.log(err));
+    });
+};
